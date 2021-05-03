255760da30f33bf5c0fcf94c24e8698a09074e66
Zichun <zichunh2000@gmail.com>
2021-05-03 10:45:22 -0700

    Add decoder

--- lgn/models/__init__.py
+++ lgn/models/__init__.py
@@ -2,4 +2,5 @@ from lgn.models.lgn_levels import LGNNodeLevel, CGMLP
 from lgn.models.lgn_cg import LGNCG
 from lgn.models.lgn_graphnet import LGNGraphNet
 from lgn.models.lgn_encoder import LGNEncoder
+from lgn.models.lgn_decoder import LGNDecoder
 from lgn.models.autotest.lgn_tests import lgn_tests
