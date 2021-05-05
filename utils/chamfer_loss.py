b0f1e67620932fc8f93a94a768c24d2a4f5d8d33
Zichun <zichunh2000@gmail.com>
2021-05-05 01:04:34 -0700

    Comments with Markdown format

--- utils/chamfer_loss.py
+++ utils/chamfer_loss.py
@@ -7,19 +7,19 @@ class ChamferLoss(nn.Module):
     ----------
     norm_choice : `str`
         The choice to compute the norm squared of the complex 4-vector.
-        Optional, default: 'cplx'
+        Optional, default: `'cplx'`
         Options:
-        - 'real'
+        - `'real'`
             1. Norm of p4 is taken so that it only contains real components.
-            Shape: (batch_size, num_particles, 4)
+            Shape: `(batch_size, num_particles, 4)`
             2. The Lorentz norm is computed using the Minkowskian metric (+, -, -, -).
             Note that the result can be negative even though it is called the 'norm squared.'
-            Shape: (batch_size, num_particles)
-        - 'cplx' or 'complex'
-            1. Lorentz norm is computed first using the Minkowskian metric (+, -, -, -), resulting in a complex number.
-            Shape: (2, batch_size, num_particles)
+            Shape: `(batch_size, num_particles)`
+        - `'cplx'` or '`complex'`
+            1. Lorentz norm is computed first using the Minkowskian metric (+, -, -, -).
+            Shape: `(2, batch_size, num_particles)`
             2. Norm of the complex number is taken. Note that the result will be non-negative.
-            Shape: (batch_size, num_particles)
+            Shape: `(batch_size, num_particles)`
     """
     def __init__(self, norm_choice='real', device=None):
         super(ChamferLoss, self).__init__()
@@ -37,10 +37,10 @@ class ChamferLoss(nn.Module):
         ------
         p : `torch.Tensor`
             The **generated** jets 4-momenta.
-            Shape: (2, batch_size, num_particles, 4).
+            Shape: `(2, batch_size, num_particles, 4)`
         q : `torch.Tensor`
             The **target** jets 4-momenta.
-            Shape: (2, batch_size, num_particles, 4) or (batch_size, num_particles, 4)
+            Shape: `(2, batch_size, num_particles, 4) or (batch_size, num_particles, 4)`
         """
 
         if (len(p.shape) != 4) or (p.shape[0] != 2):
@@ -69,14 +69,14 @@ class ChamferLoss(nn.Module):
         Inputs
         ------
         p, q: `torch.Tensor`
-            The 4-momenta of shape (2, batch_size, num_particles, 4),
+            The 4-momenta of shape `(2, batch_size, num_particles, 4)`,
             where num_particles *could* be different for p and q.
 
-        Outputs
+        Output
         -------
         dist : `torch.Tensor`
             The matrix that represents distance between each particle in p and q.
-            Shape : (batch_size, num_particles, num_particles)
+            Shape : `(batch_size, num_particles, num_particles)`
         """
         if (p.shape[1] != q.shape[1]):
             raise ValueError(f"The batch size of p and q are not equal! p.shape[0] is {p.shape[1]}, whereas q.shape[0] is {q.shape[0]}!")
@@ -107,11 +107,12 @@ def convert_to_complex(real_ps):
     -----
     real_ps : `torch.Tensor`
         Real 4-momenta of the jets.
-        Shape: (batch_size, num_particles, 4)
+        Shape: `(batch_size, num_particles, 4)`
+
     Output
     ------
     The 4-momenta extended to the field of complex numbers, with 0s in the imaginary components.
-    The shape is (2, batch_size, num_particles, 4)
+    The shape is `(2, batch_size, num_particles, 4)`
     """
     return torch.stack((real_ps, torch.zeros_like(real_ps)), dim=0)
 
@@ -119,21 +120,22 @@ def normsq_cplx(p4):
     """
     Compute the norm squared p4^2 for complex p4 and then the take the norm of the complex number.
     1. Loretnz norm is computed, resulting in a complex number.
-    Shape: (2, OTHER_DIMENSIONS)
+    Shape: `(2, OTHER_DIMENSIONS)`
     2. Norm of the complex number is taken.
-    Shape: (OTHER_DIMENSIONS)
+    Shape: `(OTHER_DIMENSIONS)`
 
     Input
     -----
     p4 : `torch.Tensor`
         The 4-momenta with shape (2, OTHER_DIMENSIONS , 4)
-        p4 = [p, q], where p is the real component, and q is the imaginary component.
+        p4 is represented by `[p, q]`, where p is the real component, and q is the imaginary component.
+
     Output
     -----
     m : `torch.Tensor`
         The norm square of p4
         Recall p^2 = - m
-        Shape: (OTHER_DIMENSIONS)
+        Shape: `(OTHER_DIMENSIONS)`
     """
     p4_real_sq = torch.pow(p4[0], 2)
     m_real = 2 * p4_real_sq[..., 0] - p4_real_sq.sum(dim=-1)
@@ -145,21 +147,22 @@ def normsq_real(p4):
     """
     Compute the norm of complex p4 and then find the Lorentz norm squared.
     1. Norm of p4 is taken so that it only contains real components.
-    Shape: (OTHER_DIMENSIONS,4)
+    Shape: `(OTHER_DIMENSIONS,4)`
     2. The Lorentz norm is computed using the Minkowskian metric (+, -, -, -).
-    Shape: (OTHER_DIMENSIONS)
+    Shape: `(OTHER_DIMENSIONS)`
 
     Input
     -----
     p4 : `torch.Tensor`
-        The 4-momenta with shape (2, ... , 4)
+        The 4-momenta with shape `(2, OTHER_DIMENSIONS, 4)`
         p4 = [p, q], where p is the real component, and q is the imaginary component.
+
     Output
     -----
     m : `torch.Tensor`
         The norm square of p4
         Recall p^2 = - m
-        Shape: (OTHER_DIMENSIONS)
+        Shape: `(OTHER_DIMENSIONS)`
     """
     p4_norm = torch.sqrt(torch.pow(p4[0], 2) + torch.pow(p4[1], 2) + 1e-12)
     return 2 * p4_norm[..., 0] - p4_norm.sum(dim=-1)
@@ -167,6 +170,6 @@ def normsq_real(p4):
 ######################################## Unused ########################################
 def reshape_generated_ps(generated_ps):
     """
-    Reshape the generated 4-momenta to (batch_size, num_particles, 2, 4), the second last axis is the complex dimension.
+    Reshape the generated 4-momenta to `(batch_size, num_particles, 2, 4)`, the second last axis is the complex dimension.
     """
     return generated_ps.permute(1,2,0,3)  # (batch_size, num_particles, 2, 4)
