1dc56d5bbdd47b59beff4fffe8c5b6a7afadf61e
Zichun Hao <zichunh2000@gmail.com>
2021-06-02 11:28:38 -0700

    Change the type of --load-to-train

--- args.py
+++ args.py
@@ -88,7 +88,7 @@ def setup_argparse():
                         help='Custom suffix of the saving directory.')
 
     # Loading existing models
-    parser.add_argument('--load-to-train', type=get_bool, default=False, metavar='',
+    parser.add_argument('--load-to-train', default=False, action='store_true', metavar='',
                         help='Whether to load existing (trained) model for training.')
     parser.add_argument('--load-path', type=str, default=None, metavar='',
                         help='Path of the trained model to load.')
