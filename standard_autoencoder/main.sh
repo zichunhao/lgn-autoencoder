b0036a1046531b4a5ea998a0aa4f87d7594b7b09
Zichun Hao <zichunh2000@gmail.com>
2021-07-14 18:20:05 -0700

    Update

--- standard_autoencoder/main.sh
+++ standard_autoencoder/main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir -p ./autoencoder-trained-models/autoencoder;
+mkdir -p ./standard-autoencoder-trained-models/autoencoder;
 python main.py \
 -b 2 \
 -j g \
 -e 5 \
 --train-fraction 2 \
 --num-valid 2 \
---save-dir autoencoder-trained-models-test \
-| tee -a "./autoencoder-trained-models-test/autoencoder-g-s1-v1-4544-4544.txt"
+--save-dir standard-autoencoder-trained-models \
+| tee -a "./standard-autoencoder-trained-models/autoencoder-g-s1-v1-4544-4544.txt"
