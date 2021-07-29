8e4d9263f65f2d2879a304ecd2a867b81220e019
Zichun Hao <zichunh2000@gmail.com>
2021-07-29 12:08:14 -0700

    Support passing args

--- main.sh
+++ main.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-output_path="./autoencoder-trained-models-test"
+if [ "$#" -eq 1 ]; then
+    output_path=$1
+    num_epochs=10
+    train_fraction=10
+    num_valid=10
+elif [[ "$#" -eq 2 ]]; then
+    output_path=$1
+    num_epochs=$2
+    train_fraction=10
+    num_valid=10
+elif [[ "$#" -eq 4 ]]; then
+    output_path=$1
+    num_epochs=$2
+    train_fraction=$3
+    num_valid=$4
+else
+    output_path="./autoencoder-trained-models-test"
+    num_epochs=10
+    train_fraction=10
+    num_valid=10
+fi
 
 mkdir -p "$output_path"/autoencoder;
 python main.py \
 -b 2 \
 -j g \
 -e 10 \
---train-fraction 10 \
---num-valid 10 \
+--train-fraction "$train_fraction" \
+--num-valid "$num_valid" \
 --maxdim 2 \
 --unit TeV \
 --loss-norm-choice p3 \
--- standard_autoencoder/main.sh
+++ standard_autoencoder/main.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-mkdir -p ./standard-autoencoder-trained-models/autoencoder;
+if [ "$#" -eq 1 ]; then
+    output_path=$1
+    num_epochs=10
+    train_fraction=10
+    num_valid=10
+elif [[ "$#" -eq 2 ]]; then
+    output_path=$1
+    num_epochs=$2
+    train_fraction=10
+    num_valid=10
+elif [[ "$#" -eq 4 ]]; then
+    output_path=$1
+    num_epochs=$2
+    train_fraction=$3
+    num_valid=$4
+else
+    output_path="./standard-autoencoder-trained-models"
+    num_epochs=10
+    train_fraction=10
+    num_valid=10
+fi
+
+mkdir -p "$output_path"/autoencoder;
 python main.py \
 -b 2 \
 -j g \
--e 5 \
---train-fraction 2 \
---num-valid 2 \
---save-dir standard-autoencoder-trained-models \
-| tee -a "./standard-autoencoder-trained-models/autoencoder-g-s1-v1-4544-4544.txt"
+-e $num_epochs \
+--train-fraction "$train_fraction" \
+--num-valid "$num_valid" \
+--save-dir "$output_path" \
+| tee -a "$output_path"/autoencoder-g-s1-v1-4544-4544.txt
