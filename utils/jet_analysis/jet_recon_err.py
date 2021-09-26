fc40e424710b2f4876b8d278b1269eb9d190f82a
Zichun Hao <zichunh2000@gmail.com>
2021-09-25 23:05:50 -0700

    Plot range based on standard deviation

--- utils/jet_analysis/jet_recon_err.py
+++ utils/jet_analysis/jet_recon_err.py
@@ -20,12 +20,12 @@ def plot_jet_recon_err(args, jet_target_cartesian, jet_gen_cartesian, jet_target
                        get_rel_err=(lambda p_target, p_gen, eps: (p_target-p_gen)/(p_target+0.01*np.median(p_target)+eps)),
                        show=False):
     """Plot reconstruction errors for jet."""
-    res_cartesian = [get_rel_err(jet_gen_cartesian[i], jet_target_cartesian[i], eps) for i in range(4)]
-    res_polar = [get_rel_err(jet_target_polar[i], jet_target_polar[i], eps) for i in range(4)]
-    ranges = get_bins(NUM_BINS)
+    rel_err_cartesian = [get_rel_err(jet_gen_cartesian[i], jet_target_cartesian[i], eps) for i in range(4)]
+    rel_err_polar = [get_rel_err(jet_target_polar[i], jet_target_polar[i], eps) for i in range(4)]
+    ranges = get_bins(NUM_BINS, rel_err_cartesian=rel_err_cartesian, rel_err_polar=rel_err_polar)
 
     LABELS = LABELS_ABS_COORD if args.abs_coord else LABELS_REL_COORD
-    for rel_err_coordinate, labels, coordinate, bin_tuple in zip((res_cartesian, res_polar), LABELS, COORDINATES, ranges):
+    for rel_err_coordinate, labels, coordinate, bin_tuple in zip((rel_err_cartesian, rel_err_polar), LABELS, COORDINATES, ranges):
         fig, axs = plt.subplots(1, 4, figsize=FIGSIZE, sharey=False)
         for ax, rel_err, bins, label in zip(axs, rel_err_coordinate, bin_tuple, labels):
             ax.hist(rel_err, bins=bins, label=get_legend(rel_err), histtype='step', stacked=True)
@@ -52,12 +52,26 @@ def default_get_rel_err(p_target, p_gen, eps, alpha=0.01):
     return (p_target - p_gen) / (p_target + alpha*np.median(p_target) + eps)
 
 
-def get_bins(
-    num_bins,
-    cartesian_min_max=((-1, 10), (-10, 10), (-10, 10), (-10, 10)),
-    polar_min_max=((-1, 10), (-1, 1.5), (-15, 15), (-15, 15)),
-):
+def get_bins(num_bins, rel_err_cartesian=None, rel_err_polar=None):
     """Get bins for jet reconstruction error plots."""
+    if rel_err_cartesian is None:
+        cartesian_min_max = ((-1, 10), (-10, 10), (-10, 10), (-10, 10))
+    else:
+        mass_min_max = (-1, -1 + 1.5 * np.std(rel_err_cartesian[0]))
+        px_min_max = (-min(1.5 * np.std(rel_err_cartesian[1]), 15), min(1.5 * np.std(rel_err_cartesian[1]), 15))
+        py_min_max = (-min(1.5 * np.std(rel_err_cartesian[2]), 15), min(1.5 * np.std(rel_err_cartesian[2]), 15))
+        pz_min_max = (-min(1.5 * np.std(rel_err_cartesian[3]), 15), min(1.5 * np.std(rel_err_cartesian[3]), 15))
+        cartesian_min_max = (mass_min_max, px_min_max, py_min_max, pz_min_max)
+
+    if rel_err_polar is None:
+        polar_min_max = ((-1, 10), (-1, 1.5), (-15, 15), (-15, 15))
+    else:
+        mass_min_max = (-1, -1 + 1.5 * np.std(rel_err_polar[0]))
+        pt_min_max = (-1, min(1.5 * np.std(rel_err_polar[1]), 15))
+        eta_min_max = (-min(1.5 * np.std(rel_err_polar[2]), 15), min(1.5 * np.std(rel_err_polar[2]), 15))
+        phi_min_max = (-min(1.5 * np.std(rel_err_polar[3]), 15), min(1.5 * np.std(rel_err_polar[3]), 15))
+        polar_min_max = (mass_min_max, pt_min_max, eta_min_max, phi_min_max)
+
     ranges_cartesian = tuple([
         np.linspace(*cartesian_min_max[i], num_bins)
         for i in range(len(cartesian_min_max))
